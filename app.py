0b03fa95ee6df17cae2240b7248bd913410f479b
Saltan71 <117144555+Saltan71@users.noreply.github.com>
2025-10-29 11:17:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ HOJA = "Sheet1"
 ESTADOS_PENDIENTES = ["Abierto"]
 
 st.set_page_config(page_title="Informe Rectauto", layout="wide")
+st.image("icono.png", width=20)
 st.title("📊 Seguimiento Equipo Regional RECTAUTO")
 
 class PDF(FPDF):
