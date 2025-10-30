6c495b978300af134d5c1a54fc42d40adcaab99c
Saltan71 <117144555+Saltan71@users.noreply.github.com>
2025-10-30 11:52:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -344,6 +344,7 @@ elif eleccion == "Envío de correos":
     st.subheader("Envío de correos")
 elif eleccion == "Indicadores clave (KPI)":
     st.subheader("Indicadores clave (KPI)")
+    Print(FECHA_REFERENCIA)
     columna_fecha = df.columns[10]
     df[columna_fecha] = pd.to_datetime(df[columna_fecha], errors='coerce')
     fecha_max = df[columna_fecha].max()
