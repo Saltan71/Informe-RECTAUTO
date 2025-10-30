b84b129a6c51ad4a342441d46593109a2e4d5eb1
Saltan71 <117144555+Saltan71@users.noreply.github.com>
2025-10-30 11:37:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -345,6 +345,10 @@ elif eleccion == "Envío de correos":
 elif eleccion == "Indicadores clave (KPI)":
     st.subheader("Indicadores clave (KPI)")
 
+    columna_fecha = df.columns[10]
+    df[columna_fecha] = pd.to_datetime(df[columna_fecha], errors='coerce')
+    fecha_max = df[columna_fecha].max()
+    
     # Crear rango de semanas
     semanas = pd.date_range(
         start=FECHA_REFERENCIA,
