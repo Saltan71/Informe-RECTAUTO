86640078bfc2d632c03be6e07dfe9acce411cc0c
Saltan71 <117144555+Saltan71@users.noreply.github.com>
2025-11-02 11:05:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ def dataframe_to_pdf_bytes(df, title):
     
     df_mostrar_pdf = df.iloc[:, :len(col_widths)]
     ALTURA_ENCABEZADO = 11
-    ALTURA_LINEA = 3  # Altura mínima por línea
+    ALTURA_LINEA = 2  # Altura mínima por línea
     ALTURA_BASE = 4   # Altura base para la primera línea
 
     def imprimir_encabezados():
