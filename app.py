43e6444c4f0a2f798816462ae7eee43c027210d8
Saltan71 <117144555+Saltan71@users.noreply.github.com>
2025-11-03 05:35:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -214,7 +214,7 @@ def dataframe_to_pdf_bytes(df_mostrar, title, df_original):
         col_widths.extend([20] * (len(df_mostrar.columns) - len(col_widths)))
     
     ALTURA_ENCABEZADO = 13
-    ALTURA_LINEA = 1.5
+    ALTURA_LINEA = 3
     ALTURA_BASE_FILA = 1.5
 
     def imprimir_encabezados():
